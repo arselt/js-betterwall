0c693bf42a69d49f922c3a663558c618b08f0447
Arath Astorga <xarselt@gmail.com>
2022-06-12 02:17:52 -0500

    ✨ Updated to a Wallpaper grid

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 const API_URL = 'https://wallhaven.cc/api/v1/search?categories=100&sorting=random'
 const CORS_FIX = 'https://cors-arselt.herokuapp.com/'
 
-async function fetchWallpaper() {
+async function fetchWallpapers() {
     const res = await fetch(CORS_FIX + API_URL)
     const data = await res.json()
 
-    const img = document.querySelector('img');
-    return img.src = data.data[0].path
+    return data.data
+    // const img = document.querySelector('img');
+    // return img.src = data.data[0].path
 }
 
-fetchWallpaper();
+const app = document.getElementById('gridApp');
+
+async function fetchGrid() {
+    const wallpapers = await fetchWallpapers();
+
+    const grid = wallpapers.map(wallpaper => {
+        return `
+            <img src=${wallpaper.thumbs.large}>
+        `
+    });
+    app.innerHTML = grid.join('');
+}
+
+fetchGrid()
+// fetchWallpaper();
